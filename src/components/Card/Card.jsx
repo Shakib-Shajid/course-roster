b02202da78a46b3763373aed47dc977a4c9440e5
Shakib-Shajid <shakibalshajid1682@gmail.com>
2023-09-14 23:07:17 +0600

    all cards added with information

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,10 +1,33 @@
+import PropTypes from 'prop-types';
 
-const Card = () => {
+const Card = ({ card }) => {
+    const { course_name, credit, price, course_details, image } = card;
     return (
-        <div>
-            <h3>Card file created</h3>
+        <div className=' bg-white border-2 border-red-400 w-80 rounded-xl p-4'>
+            <div className='flex justify-center'>
+                <img src={image} alt="" />
+            </div>
+            <h3 className='text-lg font-semibold mt-4 mb-3'>{course_name}</h3>
+            <p className='text-sm text-[#1c1b1b99] mb-4'>{course_details}</p>
+            <p className='text-base font-medium text-[#1c1b1b99]'>
+                <span>price: {price}</span>
+                <span>credit: {credit}</span>
+            </p>
+            <button className='w-full bg-[#2F80ED] py-2 text-white rounded-lg'>Select</button>
+
         </div>
     );
 };
 
+Card.propTypes = {
+    card: PropTypes.object.isRequired,
+
+}
+
 export default Card;
+
+// course_name
+// credit
+// Price
+// course_details
+// image
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,10 +1,32 @@
+import { useState } from "react";
+import { useEffect } from "react";
 import Card from "../Card/Card";
 
 const Cards = () => {
+    const [cards, setCards] = useState([]);
+
+    useEffect(() => {
+        fetch('course.json')
+            .then(res => res.json())
+            .then(data => setCards(data));
+
+    }, [])
+
+
     return (
         <div>
-            <h3>Cards File created</h3>
-            <Card />
+            <h3 className="text-2xl font-bold">Cards File created: {cards.length}</h3>
+            <div className="flex gap-6">
+                <div className="grid grid-cols-3 gap-6">
+                    {
+                        cards.map((card, idx) => <Card key={idx} card={card} />)
+                    }
+
+                </div>
+                <div>
+                    <h3>Bookmarks</h3>
+                </div>
+            </div>
         </div>
     );
 };
